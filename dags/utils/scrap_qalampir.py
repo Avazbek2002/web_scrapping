97e935830b2712bdedadb8a23dd1329e5f1bc210
Avazbek <isroilovavazbek2002@gmail.com>
2024-11-15 15:14:59 +0500

    pushed the final version of xabar and qalampir

--- dags/utils/scrap_qalampir.py
+++ dags/utils/scrap_qalampir.py
@@ -13,7 +13,7 @@ def scrap_qalampir():
         writer = csv.DictWriter(file, fieldnames=['Category', 'Title', 'Full Text'])
         writer.writeheader()
 
-        page = requests.get(news_url_list)
+        page = request_page(news_url_list)
         soup = BeautifulSoup(page.content, "html.parser")
         news_items = soup.find_all("div", class_="col-lg-4 col-md-6")
 
@@ -35,7 +35,7 @@ def scrap_qalampir():
 
                 news_url = news.find("a").get("href")
 
-                news_page = requests.get(URL + news_url)
+                news_page = request_page(URL + news_url)
                 article_soup = BeautifulSoup(news_page.content, "html.parser")
 
                 page_title = article_soup.find("div", class_="title").get_text()
@@ -72,12 +72,30 @@ def scrap_qalampir():
             }
 
             offset += load_articles
-            response = requests.get(url, params=params)
+            response = request_page(url, params=params)
             page_soup = BeautifulSoup(response.content, "html.parser")
             news_items = page_soup.find_all("div", class_="col-lg-4 col-md-6")
             print(len(news_items))
 
     print(f"Data has been written to {csv_file}")
+
+def request_page(link, params=None):
+    try:
+        extended_page = requests.get(link, params=params)
+        # Check if the request was successful
+        if extended_page.status_code == 200:
+            print("Item page fetched successfully!")
+        else:
+            print(f"Failed to fetch the item page. Status code: {extended_page.status_code}")
+
+    except requests.exceptions.ConnectionError as e:
+        print(f"Connection error occurred: {e}")
+    except requests.exceptions.Timeout as e:
+        print(f"Request timed out: {e}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+    return extended_page
 
 # Run the function
 scrap_qalampir()
