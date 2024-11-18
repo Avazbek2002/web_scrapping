86a2f17ad2782d38f4ef78d52afe3b179edf7a8e
Avazbek <isroilovavazbek2002@gmail.com>
2024-11-18 12:51:59 +0500

    pushed daryo

--- dags/utils/scrap_daryo.py
+++ dags/utils/scrap_daryo.py
@@ -8,7 +8,7 @@ def scrap_daryo():
     news_list_URL = "https://daryo.uz/yangiliklar"
 
     scrapped_data = []
-    page = requests.get(news_list_URL)
+    page = request_page(news_list_URL)
 
     soup = BeautifulSoup(page.content, "html.parser")
 
@@ -41,22 +41,15 @@ def scrap_daryo():
             # elif news_date != yesterday:
             #     stop_crawling = True
             #     break
-
-            # article_description = news.find("div", class_="excerpt")
-            # if article_description:
-            #     article_description = article_description.find("p").get_text()
-            # else:
-            #     article_description = ""
-
-            # print(article_description)
-
-            # article_link = news.find("a", class_="image-link media-ratio ratio-is-custom")
-            
-            
-            article_page = requests.get(base_URL + article_link)
+                
+            article_page = request_page(base_URL + article_link)
             log_file.write(base_URL + article_link + "\n")
 
             article_soup = BeautifulSoup(article_page.content, "html.parser")
+            
+            if article_soup.find("div", class_="not-found"):
+                print("not found")
+                continue
 
             article_category = article_soup.find("a", "category term-color-1").get_text()
 
@@ -84,12 +77,30 @@ def scrap_daryo():
 
         url = f"{news_list_URL}?page={current_page}&per-page={current_pagesize}"
         log_file.write("\n" + 20 * "-" + "\n" + "MAIN PAGE LINK: " + url + "\n")
-        page = requests.get(url)
+        page = request_page(url)
         soup = BeautifulSoup(page.content, "html.parser")
         news_items = soup.find_all("div", class_="post-meta post-meta-a has-below")
         print(len(news_items))
         current_page += 1
     
     return scrapped_data
+
+def request_page(link):
+    try:
+        extended_page = requests.get(link)
+        # Check if the request was successful
+        if extended_page.status_code == 200:
+            print("Item page fetched successfully!")
+        else:
+            print(f"Failed to fetch the item page. Status code: {extended_page.status_code}")
+
+    except requests.exceptions.ConnectionError as e:
+        print(f"Connection error occurred: {e}")
+    except requests.exceptions.Timeout as e:
+        print(f"Request timed out: {e}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+    return extended_page
 
 scrap_daryo()
