18b55c2a1c54663aba7337899c8fdacc02c6381c
Avazbek <isroilovavazbek2002@gmail.com>
2024-11-18 12:43:41 +0500

    pushed daryo final changes

--- dags/utils/scrap_daryo.py
+++ dags/utils/scrap_daryo.py
@@ -12,23 +12,35 @@ def scrap_daryo():
 
     soup = BeautifulSoup(page.content, "html.parser")
 
-    news_items = soup.find_all("article", class_="l-post grid-post grid-base-post mini__article")
-    print(len(news_items))
-    print(news_items)
+    # news_items = soup.find_all("article", class_="l-post grid-post grid-base-post mini__article")
+
+    news_items = soup.find_all("h2", class_="is-title post-title")
+
+    yesterday = datetime.now() - timedelta(days=1)
+    today = datetime.now()
+
+    yesterday = yesterday.strftime("/%Y/%m/%d")
+    today = today.strftime("/%Y/%m/%d")
 
     stop_crawling = False
     current_page = 1
     current_pagesize = 10
 
+    log_file = open("DaryoLogs.txt", "w")
+
+    log_file.write("MAIN PAGE link: " + news_list_URL + "\n")
+
     while not stop_crawling:
         for news in news_items:
-            news_date = news.find("time", class_="post-date").get_text().split(",")
+            article_link = news.find("a").get("href")
 
-            if news_date[0] == "Bugun":
-                continue
-            elif news_date[0] != "Kecha":
-                stop_crawling = True
-                break
+            news_date = article_link[:11]
+
+            # if news_date == today:
+            #     continue
+            # elif news_date != yesterday:
+            #     stop_crawling = True
+            #     break
 
             # article_description = news.find("div", class_="excerpt")
             # if article_description:
@@ -38,17 +50,17 @@ def scrap_daryo():
 
             # print(article_description)
 
-            article_link = soup.find("a", class_="image-link media-ratio ratio-is-custom").get("href")
-            # print(article_link)
+            # article_link = news.find("a", class_="image-link media-ratio ratio-is-custom")
+            
             
             article_page = requests.get(base_URL + article_link)
+            log_file.write(base_URL + article_link + "\n")
 
             article_soup = BeautifulSoup(article_page.content, "html.parser")
 
             article_category = article_soup.find("a", "category term-color-1").get_text()
 
             article_title = article_soup.find("h1", class_="is-title post-title post-view-title").get_text()
-            # print(article_title)
 
             article_paras = article_soup.find("div", class_="the-post s-post-modern").find_all("p")
 
@@ -71,9 +83,11 @@ def scrap_daryo():
 
 
         url = f"{news_list_URL}?page={current_page}&per-page={current_pagesize}"
+        log_file.write("\n" + 20 * "-" + "\n" + "MAIN PAGE LINK: " + url + "\n")
         page = requests.get(url)
         soup = BeautifulSoup(page.content, "html.parser")
         news_items = soup.find_all("div", class_="post-meta post-meta-a has-below")
+        print(len(news_items))
         current_page += 1
     
     return scrapped_data
