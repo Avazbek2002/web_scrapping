dca07a7cc787b9a9ec9a10e2960b52b3cca941c0
Avazbek <isroilovavazbek2002@gmail.com>
2024-12-13 11:18:09 +0500

    Pushed the changes in uzreport

--- dags/utils/scrap_uzreport.py
+++ dags/utils/scrap_uzreport.py
@@ -3,12 +3,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime, timedelta
 import time
 import csv
 
+# set up ChromeOptions
+options = webdriver.ChromeOptions()
 
-driver = webdriver.Chrome()
-driver_page = webdriver.Chrome()
+# add headless Chrome option
+options.add_argument("--headless=new")
+driver = webdriver.Chrome(options=options)
+driver_page = webdriver.Chrome(options=options)
 
 newslist_URL = 'https://uzreport.news/news-feed'
 URL = "https://uzreport.news/"
@@ -20,7 +25,7 @@ actions = ActionChains(driver)
 
 button = driver.find_element(By.XPATH, '//button[@class="btn default"]')
 button.click()
-news_list = driver.find_elements(By.XPATH, '//div[@class="row flex"]')
+news_list = driver.find_elements(By.XPATH, '//div[@class="heading_newsbox long"]')
 
 page_num = 1
 
@@ -33,28 +38,40 @@ with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
 
 wait = WebDriverWait(driver, 10)
 
+yesterday = (datetime.now() - timedelta(days=1)).strftime("%d")
+today = (datetime.now()).strftime("%d")
 
-while True:
+stop_scrapping = False
+
+while not stop_scrapping:
     for news in news_list:
         try:
             news_link = news.find_element(By.TAG_NAME, 'a').get_attribute("href")
-            news_date = news.find_element(By.XPATH, '//li[@class="time"]').find_element(By.TAG_NAME, "a").text
-            # news_category = news.find_element(By.XPATH, '//li[@class="rubric"]').find_element(By.TAG_NAME, 'a').text
-            news_category = wait.until(
-                EC.presence_of_element_located((By.XPATH, '//li[@class="rubric"]/a'))
-            ).text
+
+            news_date = news.find_element(By.XPATH, './/ul/li[@class="time"]/a').text.strip().split()
+            
+            # if news_date == today:
+            #     continue
+            # elif news_date != yesterday:
+            #     stop_scrapping = True
+            #     break
+
+
+            news_category = news.find_element(By.XPATH, './/ul/li[@class="rubric"]/a').text
+
             driver_page.get(news_link)
             news_title = driver_page.find_element(By.XPATH, '//div[@class="row center_panel_row"]').find_element(By.TAG_NAME, "h1").text
             paragraphs = driver_page.find_element(By.XPATH, '//div[@class="center_panel"]').find_elements(By.TAG_NAME, "p")
             page_pure_text = ""
+
             for para in paragraphs:
                 page_pure_text = page_pure_text + para.text
             
             row = {
-                        'Category': '',
-                        'Title': news_title,
-                        'Description': '',
-                        'FullText': page_pure_text
+                'Category': news_category,
+                'Title': news_title,
+                'Description': '',
+                'FullText': page_pure_text
             }
 
             # Write to CSV
